079e41a3b38d95cadb5c070b4f4ef8a30ed080d1
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-05-18 20:12:35 +0900

    init

---
+++ Memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by Soso on 2020/05/17.
+//  Copyright © 2020 Soso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
